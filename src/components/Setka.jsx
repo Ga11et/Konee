dd06737031f18f0bd804c9cd80729894dddbf139
Fyodor <sivkov1998@yandex.ru>
2022-08-21 00:13:03 +0300

    alpha

--- src/components/Setka.jsx
+++ src/components/Setka.jsx
@@ -1,45 +1,25 @@
 import SetkaSVG from '../assets/setka.svg'
-import React, { useState } from 'react'
+import React from 'react'
 import './index.css'
 import { Circle } from './Circle.jsx'
+import { useCircleControls } from '../hooks/hooks'
 
 export const Setka = ({  }) => {
 
-  const [left, setLeft] = useState(0)
-  const [top, setTop] = useState(0)
-  const [scale, setScale] = useState(20)
-
-  const rightButtonHandler = () => {
-    setLeft(prev => prev + scale < 20 ? prev + 1 : prev)
-  }
-  const leftButtonHandler = () => {
-    setLeft(prev => prev !== 0 ? prev - 1 : prev)
-  }
-  const topButtonHandler = () => {
-    setTop(prev => prev !== 0 ? prev - 1 : prev)
-  }
-  const bottomButtonHandler = () => {
-    setTop(prev => prev + 1)
-  }
-  const biggerButtonHandler = () => {
-    setScale(prev => prev !== 20 ? prev + 1 : prev)
-  }
-  const smallerButtonHandler = () => {
-    setScale(prev => prev !== 1 ? prev - 1 : prev)
-  }
+  const [values, controllers] = useCircleControls()
 
   return <>
-    <div style={{ zIndex: 5 }}>
-      <button onClick={rightButtonHandler}>Правее</button>
-      <button onClick={leftButtonHandler}>Левее</button>
-      <button onClick={topButtonHandler}>Выше</button>
-      <button onClick={bottomButtonHandler}>Ниже</button>
-      <button onClick={smallerButtonHandler}>Меньше</button>
-      <button onClick={biggerButtonHandler}>Больше</button>
+    <div className='hbContainer'>
+      <button className='hbContainer__button' onClick={controllers.right}>Правее</button>
+      <button className='hbContainer__button' onClick={controllers.left}>Левее</button>
+      <button className='hbContainer__button' onClick={controllers.up}>Выше</button>
+      <button className='hbContainer__button' onClick={controllers.down}>Ниже</button>
+      <button className='hbContainer__button' onClick={controllers.smaller}>Меньше</button>
+      <button className='hbContainer__button' onClick={controllers.bigger}>Больше</button>
     </div>
     <div className='setkaContainer'>
       <img className='disableEvents' src={SetkaSVG} alt="asd" />
-      <Circle left={left} top={top} scale={scale} />
+      <Circle left={values.left} top={values.top} scale={values.scale} />
     </div>
   </>
 }
